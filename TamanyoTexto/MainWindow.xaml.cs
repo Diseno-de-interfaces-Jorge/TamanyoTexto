36c299b9794b376c9aa3fdaa6323f4a3e88bec66
Jorge-balmis <jorge.llopis@iesdoctorbalmis.com>
2021-09-30 20:32:36 +0200

    Agregar archivos de proyecto.

---
+++ TamanyoTexto/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TamanyoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CambiarTamañoTexto(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            TextoTextBlock.FontSize = int.Parse(radioButton.Tag.ToString());
+        }
+    }
+}
